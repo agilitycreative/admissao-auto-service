26c7c6f2890be0a6d408c8e54b66418a41b31bb0
renan m <renanpontez@gmail.com>
2025-08-18 17:31:54 -0300

    feat: improve sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://admissaoautoservice.com/sitemap.xml",
+    sitemap: "https://www.admissaoautoservice.com/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://admissaoautoservice.com",
+      url: "https://www.admissaoautoservice.com",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
